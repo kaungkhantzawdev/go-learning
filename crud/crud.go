5f98cba3c09f68f510c5e47db6c6a9bc78e697ec
kaungkhantzawdev <kaungkhantzaw.dev@gmail.com>
2024-05-15 11:40:12 +0700

    finish go crud

--- crud/crud.go
+++ crud/crud.go
@@ -15,7 +15,12 @@ type Item struct {
 var items = map[string]Item{}
 
 // CREATE
-func create(w http.ResponseWriter, _ *http.Request) {
+func create(w http.ResponseWriter, r*http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method's not allowed.", http.StatusMethodNotAllowed)
+		return
+	}
 
 	var newItem Item
 
@@ -44,7 +49,7 @@ func read(w http.ResponseWriter, r*http.Request) {
 	fmt.Fprintf(w, "Item ID: %s, Name: %s, Price: %2f", item.ID, item.Name, item.Price)
 }
 
-// READ
+// READ ALL DATA
 func readAll(w http.ResponseWriter, r*http.Request) {
 
 	jsonData, err := json.Marshal(items)
@@ -58,11 +63,70 @@ func readAll(w http.ResponseWriter, r*http.Request) {
 	w.Write(jsonData)
 }
 
+// UPDATE
+func update(w http.ResponseWriter, r*http.Request) {
+
+
+	if r.Method != http.MethodPatch {
+		http.Error(w, "Method's not allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var updateItem Item
+
+	err := json.NewDecoder(r.Body).Decode(&updateItem)
+
+	if err != nil {
+		http.Error(w, "invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	itemID := updateItem.ID
+	if itemID == "" {
+		http.Error(w, "Missing item id", http.StatusBadRequest)
+	}
+
+	item, found := items[updateItem.ID]
+
+	if !found {
+		http.NotFound(w, r)
+		return
+	}
+
+	item.Name = updateItem.Name
+	item.Price = updateItem.Price
+	items[itemID] = item
+
+	fmt.Fprintln(w, item.Name)
+}
+
+// DELETE
+func remove(w http.ResponseWriter, r*http.Request) {
+
+	if r.Method != http.MethodDelete {
+		http.Error(w, "Method's not allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	itemID := r.URL.Query().Get("id")
+
+	_, found := items[itemID]
+	if !found {
+		http.NotFound(w, r)
+		return
+	}
+
+	delete(items, itemID)
+	fmt.Fprintf(w, "successfully deleted item with ID %s", itemID)
+
+}
 
 func main() {
 	http.HandleFunc("/create", create)
 	http.HandleFunc("/read", read)
 	http.HandleFunc("/read-all", readAll)
+	http.HandleFunc("/update", update)
+	http.HandleFunc("/delete", remove)
 
 	fmt.Println("Server is listening on port 8080...")
 	http.ListenAndServe(":8080", nil);
